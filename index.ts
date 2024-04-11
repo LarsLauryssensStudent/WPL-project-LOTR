5693c3ed8f949b4bce5e7d5d5cf1b892f36d9c14
Steff Van Weereld <s148156@ap.be>
2024-04-11 14:34:51 +0000

    routers opgezet van blacklist en favorites

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import tenRoundsRouter from "./routers/10-rounds";
 import suddenDeathRouter from "./routers/suddenDeath";
 import { fetchData, fullQuotes, trimCharacters, trimMovies } from "./utils";
 import { Character, Movie, Quote } from "./interFaces";
+import blacklistRouter from "./routers/blacklist";
+import favoritesRouter from "./routers/favorites";
 
 
 //arrays
@@ -12,7 +14,7 @@ export let characters: Character[] = [];
 export let quotes: Quote[] = [];
 export let movies: Movie[] = [];
 // let rawQuotes :object[] =[]
-export let tenRoundsBackgrounds :string[] = [
+export let tenRoundsBackgrounds: string[] = [
   "../assets/images/10rounds/QuestionOne.jpg",
   "../assets/images/10rounds/lotr-background-hobbiton.jpg",
   "../assets/images/10rounds/lotr-background-rivendell.jpg",
@@ -24,13 +26,13 @@ export let tenRoundsBackgrounds :string[] = [
   "../assets/images/10rounds/lotr-background-almost-mordor.jpg",
   "../assets/images/10rounds/lotr-background-mordor.jpg"
 ]
-let blackListed :Quote[] = [];
-let favorites :Quote[] = [];
+let blackListed: Quote[] = [];
+let favorites: Quote[] = [];
 
 // vars
 // let gameMode :string = "selection";
-let qCounter :number = 0;
-let randomQuote :Quote;
+let qCounter: number = 0;
+let randomQuote: Quote;
 // let userScore :number = 0;
 //index export functies
 
@@ -40,23 +42,23 @@ export function setQCounter(value: number) {
 export function getQCounter() {
   return qCounter;
 }
-export function setNewQuote(array:Quote[]) {
-  let randomIndex :number = Math.floor(Math.random() * quotes.length);
+export function setNewQuote(array: Quote[]) {
+  let randomIndex: number = Math.floor(Math.random() * quotes.length);
   randomQuote = array[randomIndex];
 }
-export function returnQuote():Quote {
+export function returnQuote(): Quote {
   return randomQuote;
 }
-export function getBlacklist():Quote[] {
+export function getBlacklist(): Quote[] {
   return blackListed;
 }
-export function addToBlacklist(quote :Quote) {
+export function addToBlacklist(quote: Quote) {
   blackListed.push(quote);
 }
-export function getFavorites() :Quote[] {
+export function getFavorites(): Quote[] {
   return favorites;
 }
-export function addToFavorites(quote :Quote) {
+export function addToFavorites(quote: Quote) {
   favorites.push(quote);
 }
 
@@ -68,17 +70,19 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 //routes
 app.use("/selection", selectionRouter());
 app.use("/10-Rounds", tenRoundsRouter());
 app.use("/Sudden-Death", suddenDeathRouter());
+app.use("/Blacklist", blacklistRouter());
+app.use("/Favorites", favoritesRouter());
 
 
 //index
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.render("index");
 });
 
@@ -94,17 +98,17 @@ app.get("/", (req, res)=>{
 
 app.listen(app.get("port"), async () => {
   try {
-  const data :any = await fetchData();
+    const data: any = await fetchData();
 
-  quotes = await fullQuotes(data.quotesTT, data.quotesFS, data.quotesRK);
-  characters = await trimCharacters(data.characters, quotes);
-  movies = await trimMovies(data.movies, quotes);
-  setNewQuote(quotes);
-  
-  console.log(`Port running on ${app.get("port")}`);
+    quotes = await fullQuotes(data.quotesTT, data.quotesFS, data.quotesRK);
+    characters = await trimCharacters(data.characters, quotes);
+    movies = await trimMovies(data.movies, quotes);
+    setNewQuote(quotes);
+
+    console.log(`Port running on ${app.get("port")}`);
   } catch (error) {
     console.error("Error fetching data:", error);
-  console.log(`Port running on ${app.get("port")}`);
+    console.log(`Port running on ${app.get("port")}`);
 
   }
 });
---
+++ routers/favorites.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+export default function favoritesRouter() {
+    const router = express.Router();
+
+    router.get("/", (req, res) => {
+        res.render("favorites")
+    });
+    return router
+}
