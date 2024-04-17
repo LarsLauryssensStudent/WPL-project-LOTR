521debd870205f3696398b0be7601ebdc213d763
Steff Van Weereld <s148156@ap.be>
2024-04-17 10:35:40 +0000

    afwerking

--- routers/10-rounds.ts
+++ routers/10-rounds.ts
@@ -3,13 +3,13 @@ import { fetchData, generatePossibleAnswers, shuffleArray } from "../utils";
 import { getQCounter, setQCounter, movies, quotes, characters, tenRoundsBackgrounds, returnQuote, setNewQuote, addToBlacklist, getBlacklist, getFavorites, toggleFavorites } from "../index";
 import { Quote, Movie, Character } from "../interfaces";
 
-export let score :number = 0;
+export let score: number = 0;
 
 
 export default function tenRoundsRouter() {
     const router = express.Router();
 
-    router.get("/", async (req,res) => {
+    router.get("/", async (req, res) => {
         if (getQCounter() > 10) {
             setQCounter(1);
             score = 0;
@@ -20,62 +20,62 @@ export default function tenRoundsRouter() {
                 score = 0;
             }
             //quote randomen
-        const randomQuote :Quote = returnQuote();
-        const randomChars = generatePossibleAnswers(randomQuote, characters);
+            const randomQuote: Quote = returnQuote();
+            const randomChars = generatePossibleAnswers(randomQuote, characters);
             // movie randomen
-        const shuffledMovies = shuffleArray(movies);
-        console.log(getQCounter());
-        res.render("quizzNormal", {       
-            backgroundUrl: tenRoundsBackgrounds[getQCounter() -1],
-            qCounter: getQCounter(), 
-            quote: randomQuote,
-            characters: randomChars,
-            movies: shuffledMovies,
-            score:score
-        });
-      
-    }
+            const shuffledMovies = shuffleArray(movies);
+            console.log(getQCounter());
+            res.render("quizzNormal", {
+                backgroundUrl: tenRoundsBackgrounds[getQCounter() - 1],
+                qCounter: getQCounter(),
+                quote: randomQuote,
+                characters: randomChars,
+                movies: shuffledMovies,
+                score: score
+            });
+
+        }
     })
 
-    router.get("/check", async (req,res) => {
-        let characterChoice :string = typeof req.query.actorRadio === "string" ? req.query.actorRadio : "";
-        let movieChoice :string = typeof req.query.movieRadio === "string" ? req.query.movieRadio : "";
+    router.get("/check", async (req, res) => {
+        let characterChoice: string = typeof req.query.actorRadio === "string" ? req.query.actorRadio : "";
+        let movieChoice: string = typeof req.query.movieRadio === "string" ? req.query.movieRadio : "";
         console.log(characterChoice, movieChoice);
-        let prevQuote :Quote = returnQuote();
-        let correctChar :string = prevQuote.character;
-        let correctMovie :string = prevQuote.movie;
+        let prevQuote: Quote = returnQuote();
+        let correctChar: string = prevQuote.character;
+        let correctMovie: string = prevQuote.movie;
         if (characterChoice === correctChar) {
             score += 1;
         }
         if (movieChoice === correctMovie) {
             score += 1;
         }
-      
+
 
         setNewQuote(quotes);
-        let current :number = getQCounter();
+        let current: number = getQCounter();
         current++;
         setQCounter(current);
-        
+
         res.redirect("/10-Rounds");
-        
+
 
     })
 
-    router.get("/blacklist", (req,res) => {
-        let currentQuote :Quote = returnQuote();
+    router.get("/blacklist", (req, res) => {
+        let currentQuote: Quote = returnQuote();
         addToBlacklist(currentQuote);
         setNewQuote(quotes);
         console.log(getBlacklist());
-        res.redirect("/10-Rounds");      
+        res.redirect("/10-Rounds");
     })
-    router.get("/favorites", (req,res) => {
-        let currentQuote :Quote = returnQuote();
-        let favorites : Quote[] = getFavorites();
+    router.get("/favorites", (req, res) => {
+        let currentQuote: Quote = returnQuote();
+        let favorites: Quote[] = getFavorites();
         toggleFavorites(currentQuote);
         res.redirect("/10-Rounds");
 
     });
 
-    return router 
+    return router
 }
