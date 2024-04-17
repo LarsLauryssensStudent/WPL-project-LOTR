5fe034b110d8d9b7532580f50f8ebb5e49c22771
Steff Van Weereld <s148156@ap.be>
2024-04-17 07:17:06 +0000

    test

--- utils.ts
+++ utils.ts
@@ -3,81 +3,81 @@ import { characters } from ".";
 import { Character, Movie, Quote } from "./interfaces";
 
 export async function fetchData() {
-    //token
-    const token = 'mIqYC2hqv_DXksfzJsvn ';
-    const headers = {
-      'Authorization': `Bearer ${token}`
-    };
-  
-    //error handling voor quotes, chars en movies
-    try {
-      const dataQuotesTwoTowers = await fetch("https://the-one-api.dev/v2//movie/5cd95395de30eff6ebccde5b/quote", { headers });
-      if (!dataQuotesTwoTowers.ok) {
-        throw new Error(`Failed to fetch quotes: ${dataQuotesTwoTowers.statusText}`);
-      }
-      const responseData = await dataQuotesTwoTowers.json();
-      const quotesTT = responseData.docs;
-      
-      
-      const dataQuotesFellowShip = await fetch("https://the-one-api.dev/v2//movie/5cd95395de30eff6ebccde5c/quote", { headers });
-      if (!dataQuotesFellowShip.ok) {
-        throw new Error(`Failed to fetch quotes: ${dataQuotesFellowShip.statusText}`);
-      }      
-      const responseDataFellowShip = await dataQuotesFellowShip.json();
-      const quotesFS = responseDataFellowShip.docs; 
-      
-      const dataQuotesReturnKing = await fetch("https://the-one-api.dev/v2//movie/5cd95395de30eff6ebccde5d/quote", { headers });
-      if (!dataQuotesReturnKing.ok) {
-        throw new Error(`Failed to fetch quotes: ${dataQuotesReturnKing.statusText}`);
-      }
-      const responseDataReturnKing = await dataQuotesReturnKing.json();
-      const quotesRK = responseDataReturnKing.docs;
+  //token
+  const token = 'mIqYC2hqv_DXksfzJsvn ';
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  };
 
-
-
-      const dataChars = await fetch("https://the-one-api.dev/v2/character", { headers });
-      if (!dataChars.ok) {
-        throw new Error(`Failed to fetch characters: ${dataChars.statusText}`);
-      }
-      const tempchar = await dataChars.json();
-      const characters = tempchar.docs;
-     
-
-      const dataMovies = await fetch("https://the-one-api.dev/v2/movie", { headers });
-      if (!dataMovies.ok) {
-        throw new Error(`Failed to fetch movies: ${dataMovies.statusText}`);
-      }
-      const temp = await dataMovies.json();
-      const movies = temp.docs;
-      //return van opgehaalde data
-      return {
-        quotesTT,
-        quotesFS,
-        quotesRK,
-        characters,
-        movies
-      };
-
-    } catch (error: any) {
-      throw new Error(`Could not fetch data: ${error.message}`);
+  //error handling voor quotes, chars en movies
+  try {
+    const dataQuotesTwoTowers = await fetch("https://the-one-api.dev/v2//movie/5cd95395de30eff6ebccde5b/quote", { headers });
+    if (!dataQuotesTwoTowers.ok) {
+      throw new Error(`Failed to fetch quotes: ${dataQuotesTwoTowers.statusText}`);
     }
-  }
+    const responseData = await dataQuotesTwoTowers.json();
+    const quotesTT = responseData.docs;
 
-export function fullQuotes (array1: Quote[], array2: Quote[], array3: Quote[]): Quote[] {
-  let quotes :Quote[]= [];
+
+    const dataQuotesFellowShip = await fetch("https://the-one-api.dev/v2//movie/5cd95395de30eff6ebccde5c/quote", { headers });
+    if (!dataQuotesFellowShip.ok) {
+      throw new Error(`Failed to fetch quotes: ${dataQuotesFellowShip.statusText}`);
+    }
+    const responseDataFellowShip = await dataQuotesFellowShip.json();
+    const quotesFS = responseDataFellowShip.docs;
+
+    const dataQuotesReturnKing = await fetch("https://the-one-api.dev/v2//movie/5cd95395de30eff6ebccde5d/quote", { headers });
+    if (!dataQuotesReturnKing.ok) {
+      throw new Error(`Failed to fetch quotes: ${dataQuotesReturnKing.statusText}`);
+    }
+    const responseDataReturnKing = await dataQuotesReturnKing.json();
+    const quotesRK = responseDataReturnKing.docs;
+
+
+
+    const dataChars = await fetch("https://the-one-api.dev/v2/character", { headers });
+    if (!dataChars.ok) {
+      throw new Error(`Failed to fetch characters: ${dataChars.statusText}`);
+    }
+    const tempchar = await dataChars.json();
+    const characters = tempchar.docs;
+
+
+    const dataMovies = await fetch("https://the-one-api.dev/v2/movie", { headers });
+    if (!dataMovies.ok) {
+      throw new Error(`Failed to fetch movies: ${dataMovies.statusText}`);
+    }
+    const temp = await dataMovies.json();
+    const movies = temp.docs;
+    //return van opgehaalde data
+    return {
+      quotesTT,
+      quotesFS,
+      quotesRK,
+      characters,
+      movies
+    };
+
+  } catch (error: any) {
+    throw new Error(`Could not fetch data: ${error.message}`);
+  }
+}
+
+export function fullQuotes(array1: Quote[], array2: Quote[], array3: Quote[]): Quote[] {
+  let quotes: Quote[] = [];
   console.log(array1.length)
   console.log(array2.length)
   console.log(array3.length)
 
-  for(let i: number = 0; i < array1.length ; i++) {
+  for (let i: number = 0; i < array1.length; i++) {
     quotes.push(array1[i]);
- 
+
   }
-  for(let j: number = 0; j < array2.length ; j++) {
+  for (let j: number = 0; j < array2.length; j++) {
     quotes.push(array2[j]);
-    
+
   }
-  for(let k: number = 0; k < array3.length ; k++) {
+  for (let k: number = 0; k < array3.length; k++) {
     quotes.push(array3[k]);
 
   }
@@ -86,7 +86,7 @@ export function fullQuotes (array1: Quote[], array2: Quote[], array3: Quote[]): 
   return quotes;
 }
 
-export function trimCharacters (array: Character[], quotes :Quote[]) :Character[] {
+export function trimCharacters(array: Character[], quotes: Quote[]): Character[] {
   const characterIds = [...new Set(quotes.map(quote => quote.character))];
   console.log(characterIds.length);
 
@@ -95,7 +95,7 @@ export function trimCharacters (array: Character[], quotes :Quote[]) :Character[
   return filteredCharacters;
 }
 
-export function trimMovies (array : Movie[], quotes :Quote[]) :Movie[] {
+export function trimMovies(array: Movie[], quotes: Quote[]): Movie[] {
   const movieIds = [...new Set(quotes.map(quote => quote.movie))];
   console.log(movieIds.length);
 
@@ -106,8 +106,8 @@ export function trimMovies (array : Movie[], quotes :Quote[]) :Movie[] {
 // shuffle algoritme van fisher yates
 export function shuffleArray<T>(array: T[]): T[] {
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
@@ -123,7 +123,7 @@ export function generatePossibleAnswers(quote: Quote, array: Character[]): Chara
   const correctCharacter = shuffledCharacters.find(character => character._id === correctCharacterId);
   if (!correctCharacter) {
     throw new Error("Correct character not found");
-}
+  }
   // en dan 2 randoms
   const otherCharacters = shuffledCharacters.filter(character => character._id !== correctCharacterId).slice(0, 2);
   //samenvoegen
@@ -133,11 +133,11 @@ export function generatePossibleAnswers(quote: Quote, array: Character[]): Chara
   return shuffleArray(possibleAnswers);
 }
 
-export function linkQuotes(quotes: Quote[], char: Character[]): Character[]{
-  let character:Character[] = [];
+export function linkQuotes(quotes: Quote[], char: Character[]): Character[] {
+  let character: Character[] = [];
   quotes.forEach(q => {
-   const linked = char.find(c => c._id === q._id);
-    if(linked !== undefined){
+    const linked = char.find(c => c._id === q.character);
+    if (linked !== undefined) {
       character.push(linked);
     }
   });
